36cb2100508f607506fcad75aae6fdabd10df32e
Админ <admin@MacBook-Pro-Admin.local>
2023-08-07 19:58:48 +0300

    add: shadcnui gsap fonts

---
+++ components/ui/container.tsx
@@ -0,0 +1,8 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+export const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+  return (
+    <div className={cn(`flex justify-center items-center w-full max-w-[1832px] px-4 mx-auto`, className)}>{children}</div>
+  )
+}
